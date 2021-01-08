8fdc38aed7702606ae020f0a7ca781e8f72b4b80
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-07 21:51:15 -0300

    Finalizando o front das paginas.

--- site/components/menu.js
+++ site/components/menu.js
@@ -1,60 +1,42 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
 } from 'reactstrap';
 
-const Example = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Menu = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+    const toggle = () => setIsOpen(!isOpen);
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+    return (
+        <div>
+
+            <Navbar color="primary" dark expand="md">
+                <Container>
+                    <NavbarBrand href="/">Inicio</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">listar</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/cadastrar/">Cadastrar</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
 }
 
-export default Example;
+export default Menu;
--- site/pages/index.js
+++ site/pages/index.js
@@ -1,16 +1,41 @@
+import Menu from '../components/menu';
+
+import { Jumbotron, Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
+
 export default function HomePage({ data }) {
     return (
         <>
-            <h1>Minhas Metas!!</h1> <hr />
-            {   data.metas.map(meta => (
-                <div key={meta._id}>
-                    <h2> {meta.name} </h2>
-                    <p> {meta.description} </p>
-                    <p> {meta.status} </p>
-                    <hr />
-
-                </div>
-            ))}
+            <Menu />
+            <Jumbotron fluid className="list">
+                <style>
+                    {`.list{
+                         background-color: #171941;
+                         padding-top: 30px;
+                         padding-bottom: 241px;
+                         margin-bottom: 0rem !important;
+                    }.title-top{
+                        color: #bf38ac;
+                    }.list-meta{
+                        background-color: #0d345d !important;
+                        border-color: #4a0242;
+                        color: #fff;
+                    }`}
+                </style>
+                <Container>
+                    <h1 className="display-4 text-center title-top">Minhas Metas!!</h1> <hr />
+                    <ListGroup>
+                        {data.metas.map(meta => (
+                            <div key={meta._id}>
+                                <ListGroupItem className="list-meta">
+                                    <ListGroupItemHeading>{meta.name}</ListGroupItemHeading>
+                                    <ListGroupItemText> {meta.description} </ListGroupItemText>
+                                    <ListGroupItemText> {meta.status} </ListGroupItemText>
+                                </ListGroupItem>
+                            </div>
+                        ))}
+                    </ListGroup>
+                </Container>
+            </Jumbotron>
         </>
     )
 };
